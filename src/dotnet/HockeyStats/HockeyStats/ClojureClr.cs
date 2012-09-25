f85bd517f1e649bbd8d48558b792a885491f9cd0
Rob Rowe <rippinrobr@hotmail.com>
2012-09-25 11:01:08 -0400

    updating the testcmd project

---
+++ src/dotnet/HockeyStats/HockeyStats/ClojureClr.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HockeyStats
+{
+    public static class ClojureClr
+    {
+        public static void GetHtml()
+        {
+            Console.WriteLine(export.html.CreateHtml("Ricci"));
+        }
+    }
+}
