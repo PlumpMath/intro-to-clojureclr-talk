6ca2fd38adbd46ef287a792e91ebbe329652df79
Rob Rowe <rippinrobr@hotmail.com>
2012-10-06 14:25:29 -0400

    Updated the repl.clj file to reflect what we did during the talk.

--- src/repl.clj
+++ src/repl.clj
@@ -1,27 +1,43 @@
-(System.Reflection.Assembly/LoadWithPartialName "System.Windows.Forms")
-(ns repl
-  (:import [System.Windows.Forms MessageBox]))
+(ns repl)
 
+;; -----------------------------
+;; Basics
+;; -----------------------------
+(def msg "Clojure is Cool!")
 
-(defn call-static-method 
-  "This function illustrates how to call a static .NET function"
-  []
-  (System.Console/WriteLine "I just called a .NET method!"))
+(defn echo [my-str]
+  (println "You said:" my-str))
 
+(System.Console/WriteLine "I just called a .NET method!")
 
-(defn call-message-box
-  "This function loads the Windows.Forms assembly, imports the 
-   System.Windows.Forms.MessageBox namespace and then calls
-   MessageBox.Show displaying the message that was passed in.
+;; ------------------------------
+;; Sequences
+;; ------------------------------
+ (def numbers (range 10))
+ 
+ (map println numbers)
+ (apply + numbers)
+ (filter evin? numbers)
 
-   In a 'normal' app you would have the System.Reflection.Assembly/Load
-   at the top of the source file and the import would come after the ns
-   call.  I have them here for demo purposes only."
-   [msg]
-    (System.Reflection.Assembly/Load "System.Windows.Forms, Version=2.0.0.0, 
-                Culture=neutral, PublicKeyToken=b77a5c561934e089")
-    (import (System.Windows.Forms MessageBox))
-    (MessageBox/Show msg  "ClojureCLR Dialog"))
+;; ------------------------------
+;; .NET - My Own Assembly
+;; ------------------------------
+ (assembly-load-from "deps\\ReplExample.dll")
+ (def obj (new ReplExample.MyClass)) 
+ 
+ (set! (.MyVal obj) 10)
+ (prinltn (.MyVal obj))
+ (.GetTime obj)
+ (.SayHi obj “Richmond”)
+ 
+;; ------------------------------
+;; .NET Assemblies
+;; ------------------------------
+ (System.Reflection.Assembly/LoadWithPartialName "System.Windows.Forms")
+ (import [System.Windows.Forms MessageBox])
+
+ (defn display-message [str] 
+    (MessageBox/Show str "ClojureCLR Dialog"))
 
 
 
